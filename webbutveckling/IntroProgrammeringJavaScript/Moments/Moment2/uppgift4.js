ce14ff7a927b451fb979e2888825ce1b189e68a2
EliasEriksson <EliasEriksson95@gmail.com>
2020-09-12 17:07:27 +0200

    wrote even ment odd

--- webbutveckling/IntroProgrammeringJavaScript/Moments/Moment2/uppgift4.js
+++ webbutveckling/IntroProgrammeringJavaScript/Moments/Moment2/uppgift4.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// looping over a range of numbers and logging all the even ones.
+// looping over a range of numbers and logging all the odd ones.
 for (let i = 3; i <= 23; i++) {
     if (i % 2 === 1) {
         console.log(i);
