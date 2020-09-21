10e555287b3160e0c432a7d28e8a6c40be82276d
EliasEriksson <EliasEriksson95@gmail.com>
2020-09-22 00:37:28 +0200

    finished codebase for Moment 3. still needs comments. might do refactoring.

--- webbutveckling/IntroProgrammeringJavaScript/Moments/Moment3/js/main.js
+++ webbutveckling/IntroProgrammeringJavaScript/Moments/Moment3/js/main.js
@@ -1,19 +1,73 @@
 "use strict";
 
-console.log("loaded JS file.")
+let newToDo = document.getElementById("newtodo");
+let newToDoButton = document.getElementById("newtodobutton");
+let clearButton = document.getElementById("clearbutton");
+let toDoList = document.getElementById("todolist");
+let errorMessage = document.getElementById("message");
 
-window.addEventListener("load", (_) => {
-    console.log("page loaded.");
+let storedToDos = JSON.parse(localStorage.getItem("articles"));
+if (!storedToDos) {
+    storedToDos = [];
+}
+
+function storeToDo(text) {
+    storedToDos.push(text);
+    localStorage.setItem("articles", JSON.stringify(storedToDos));
+}
+
+function removeToDo(text) {
+    storedToDos.splice(storedToDos.indexOf(text), 1);
+    localStorage.setItem("articles", JSON.stringify(storedToDos));
+}
+
+function clearToDos() {
+    toDoList.innerHTML = "";
+    localStorage.setItem("articles", null);
+    storedToDos = [];
+}
+
+function addToDo(text) {
+    let articleNode = document.createElement("article");
+    articleNode.addEventListener("click", () => {
+        removeToDo(text)
+        articleNode.remove();
+    })
+
+    let textNode = document.createTextNode(text);
+    articleNode.appendChild(textNode);
+    toDoList.appendChild(articleNode);
+}
+
+function inputIsValid(text) {
+    if (text.length > 4) {
+        errorMessage.innerHTML = "";
+        return true;
+    }
+    errorMessage.innerHTML = "Ange minst fem tecken.";
+    return false;
+}
+
+newToDo.addEventListener("input", () => {
+    inputIsValid(newToDo.value);
+})
+
+newToDoButton.addEventListener("click", () => {
+    let text = newToDo.value;
+    if (inputIsValid(text)) {
+        addToDo(text);
+        storeToDo(text);
+        newToDo.value = "";
+    }
 });
 
-let newToDoButton = document.getElementById("newtodobutton");
-newToDoButton.onclick = () => {
-    let newToDo = document.getElementById("newtodo");
-    console.log(newToDo);
-    alert("wat?")
-}
+clearButton.addEventListener("click", () => {
+    clearToDos();
+})
 
-let clearButton = document.getElementById("clearbutton");
-newToDoButton.onclick = () => {
+window.addEventListener("load", () => {
+    for (let i = 0; i < storedToDos.length; i++) {
+        addToDo(storedToDos[i]);
+    }
+})
 
-}
