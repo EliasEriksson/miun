9e9cb6ccdbbaa39fe6d2fec7ee580ab9a1c64681
EliasEriksson <EliasEriksson95@gmail.com>
2020-09-22 10:33:07 +0200

    finished codebase for Moment 3. still needs comments. might do refactoring.

--- webbutveckling/IntroProgrammeringJavaScript/Moments/Moment3/js/main.js
+++ webbutveckling/IntroProgrammeringJavaScript/Moments/Moment3/js/main.js
@@ -1,36 +1,46 @@
 "use strict";
 
+// storing all relevant elements in global scope so they only have to be queried once.
 let newToDo = document.getElementById("newtodo");
 let newToDoButton = document.getElementById("newtodobutton");
 let clearButton = document.getElementById("clearbutton");
 let toDoList = document.getElementById("todolist");
 let errorMessage = document.getElementById("message");
 
+// loads all stored to-dos in memory
 let storedToDos = JSON.parse(localStorage.getItem("articles"));
 if (!storedToDos) {
     storedToDos = [];
 }
 
 function storeToDo(text) {
+    /**
+     * adds a to-do to the memory and save the memory to localstorage.
+     */
     storedToDos.push(text);
     localStorage.setItem("articles", JSON.stringify(storedToDos));
 }
 
-function removeToDo(text) {
-    storedToDos.splice(storedToDos.indexOf(text), 1);
-    localStorage.setItem("articles", JSON.stringify(storedToDos));
-}
-
 function clearToDos() {
+    /**
+     * removes all todos from memory, the website and stores null to the localstorage.
+     */
     toDoList.innerHTML = "";
     localStorage.setItem("articles", null);
     storedToDos = [];
 }
 
 function addToDo(text) {
+    /**
+     * adds a to-do to the website
+     *
+     * the to-do item also gets an anonymous function for its onclick event
+     * that removes the item from the website, memory and localstorage if clicked.
+     */
     let articleNode = document.createElement("article");
     articleNode.addEventListener("click", () => {
-        removeToDo(text)
+        storedToDos.splice(storedToDos.indexOf(text), 1);
+        localStorage.setItem("articles", JSON.stringify(storedToDos));
         articleNode.remove();
     })
 
@@ -40,6 +50,11 @@ function addToDo(text) {
 }
 
 function inputIsValid(text) {
+    /**
+     * validates text and returns true if its valid
+     *
+     * if it returns false an error message is set to the message element.
+     */
     if (text.length > 4) {
         errorMessage.innerHTML = "";
         return true;
@@ -49,10 +64,21 @@ function inputIsValid(text) {
 }
 
 newToDo.addEventListener("input", () => {
+    /**
+     * listener for changes in teh input field
+     *
+     * if fired check if input is valid.
+     */
     inputIsValid(newToDo.value);
 })
 
 newToDoButton.addEventListener("click", () => {
+    /**
+     * listener for click events on the "lägg till" button
+     *
+     * when fired adds the input fields text to website,
+     * memory and local storage if input validates.
+     */
     let text = newToDo.value;
     if (inputIsValid(text)) {
         addToDo(text);
@@ -62,10 +88,21 @@ newToDoButton.addEventListener("click", () => {
 });
 
 clearButton.addEventListener("click", () => {
+    /**
+     * listener for click events on the "rensa" button
+     *
+     * when fired all the to-dos are cleared from the website,
+     * memory and localstorage.
+     */
     clearToDos();
 })
 
 window.addEventListener("load", () => {
+    /**
+     * listener for when the page is finished loading.
+     *
+     * adds all the stored to-dos in localstorage to the site.
+     */
     for (let i = 0; i < storedToDos.length; i++) {
         addToDo(storedToDos[i]);
     }
