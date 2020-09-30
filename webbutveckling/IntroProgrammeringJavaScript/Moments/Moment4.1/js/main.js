e41cffc6beb80e7ea9d051755ad5c31bee6605f9
EliasEriksson <EliasEriksson95@gmail.com>
2020-09-30 10:26:23 +0200

    finished

--- webbutveckling/IntroProgrammeringJavaScript/Moments/Moment4.1/js/main.js
+++ webbutveckling/IntroProgrammeringJavaScript/Moments/Moment4.1/js/main.js
@@ -1,23 +1,25 @@
 let output = document.getElementById("output");
 
-function newMovie(title, category, playtime){
-    return {
-        "title": title,
-        "category": category,
-        "playtime": playtime,
-        "getInformation": function() {
-            return `${this.title}, ${this.category}, ${this.playtime} minuter.`
-        }
+
+function Movie(title, category, playTime) {
+    this.title = title;
+    this.category = category;
+    this.playtime = playTime;
+    this.getInformation = function() {
+        return `${this.title}, ${this.category}, ${this.playtime} minuter`
     };
 }
 
-let movie1 = newMovie("Star Wars", "Science fiction", 104);
-let movie2 = newMovie("Introduktion till JavaScript", "Utbildning", 24);
-let movie3 = newMovie("Borta med vinden", "Drama", 210);
-let movie4 = newMovie("Jagad", "Thriller", 105);
+let movie1 = new Movie("Star Wars", "Science fiction", 104);
+let movie2 = new Movie("Introduktion till JavaScript", "Utbildning", 24);
+let movie3 = new Movie("Borta med vinden", "Drama", 210);
+let movie4 = new Movie("Jagad", "Thriller", 105);
 
 let movies = [
     movie1, movie2, movie3, movie4
 ]
 
-output.innerHTML += movies.map(movie => movie.getInformation()).join("\n");
+console.log(movie1)
+
+
+output.innerHTML = movies.map(movie => movie.getInformation()).join("\n");
