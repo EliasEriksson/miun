3a719fa65971f6d8ba6b05c86ac827c80d366d37
EliasEriksson <EliasEriksson95@gmail.com>
2021-01-05 11:34:27 +0100

    some changes

---
+++ webbutveckling/DigitalBildbehandling/Moment4/Site/static/js/main.js
@@ -0,0 +1,53 @@
+let cartButtonElement = document.getElementById("cartButton");
+let cartContentElement = document.getElementById("cartContent");
+
+let mainMenuButtonElement = document.getElementById("main-menu-button");
+let mainNavigationElement = document.getElementById("main-navigation");
+
+let searchButtonElement = document.getElementById("search-button")
+let lastScroll = 0;
+
+// to hide the search bar
+window.addEventListener("scroll", function (){
+    if (window.pageYOffset > lastScroll) {
+        // scroll up
+        console.log("scroll down")
+    } else {
+        console.log("scroll up")
+        // scroll down
+    }
+})
+
+// show the cart when clicking
+cartButtonElement.addEventListener("click", function () {
+    if (cartContentElement.style.display === "none" || cartContentElement.style.display === "") {
+        cartContentElement.style.display = "block";
+    } else {
+        cartContentElement.style.display = "none";
+    }
+});
+
+// hide the card when it loses focus
+cartButtonElement.addEventListener("focusout", function () {
+    cartContentElement.style.display = "none";
+});
+
+// toggle main menu
+mainMenuButtonElement.addEventListener("click", function () {
+    if (mainNavigationElement.style.display === "none" || mainNavigationElement.style.display === "") {
+        mainNavigationElement.style.display = "flex";
+    } else {
+        mainNavigationElement.style.display = "none";
+    }
+});
+
+// redirect to search
+searchButtonElement.addEventListener("click", function (e) {
+    e.preventDefault();
+    let searchFieldElement = document.getElementById("search-field");
+    let searchPhrase = searchFieldElement.value;
+    let newURL = window.location.href.split("?")[0]
+               + searchFieldElement.parentElement.getAttribute("action")
+               + `?${searchPhrase}`;
+    window.location.replace(newURL);
+});
---
+++ webbutveckling/DigitalBildbehandling/Moment4/Site/static/js/search.js
@@ -0,0 +1,87 @@
+// assumes the user to be on a link ending with ?SEARCH_PHRASE_HERE
+
+class Product {
+    constructor(id, price, thumbnail, thumbnailAltText) {
+        this.id = id;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.thumbnailAltText = thumbnailAltText;
+    }
+}
+
+let products = {
+    "Rakt Champagneglas": new Product("c0b5c33c", 100, "../media/glass.jpg"),
+    "Litet kupat Dricksglas": new Product("d4d35b3e", 100, "../media/glass.jpg"),
+    "Kupat Dricksglas": new Product("d7000c2e", 100, "../media/glass.jpg"),
+    "Rakt Dricksglas": new Product("dd4bd193", 100, "../media/glass.jpg"),
+    "Kupat Vinglas": new Product("v74f32ec", 100, "../media/glass.jpg"),
+    "Vinglas": new Product("vf380160", 100, "../media/glass.jpg")
+}
+
+function removeFileExtension(path) {
+    let lastIndex = path.lastIndexOf(".");
+    return path.substring(0, lastIndex);
+}
+
+function generateSource(product, extension) {
+    let sourceElement = document.createElement("source");
+
+    sourceElement.setAttribute("srcset", `${removeFileExtension(product.thumbnail)}.${extension}`);
+    sourceElement.setAttribute("type", `image/${extension}`);
+    return sourceElement;
+}
+
+function generateImg(product) {
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", product.thumbnail);
+    imgElement.setAttribute("alt", product.thumbnailAltText);
+    return imgElement;
+}
+
+function generateThumbnail(product) {
+    let pictureElement = document.createElement("picture");
+    pictureElement.appendChild(generateSource(product, "avif"));
+    pictureElement.appendChild(generateSource(product, "webp"));
+    pictureElement.appendChild(generateImg(product));
+    return pictureElement
+}
+
+function generateProductInfo(productName) {
+    let pElement = document.createElement("p");
+    pElement.innerHTML = productName;
+    pElement.classList.add("card-product-info");
+    return pElement;
+}
+
+function generatePrice(product) {
+    let pElement = document.createElement("p");
+    pElement.innerHTML = product.price;
+    pElement.classList.add("card-product-price");
+    return pElement;
+}
+
+function addSearchResult(productName, product, searchResultElement) {
+    let productWrapperElement = document.createElement("div");
+    productWrapperElement.classList.add("product-wrapper");
+
+    let productElement = document.createElement("a");
+    productElement.setAttribute("href", `../Glas/${product.id}/`)
+    productElement.classList.add("product");
+
+    productElement.appendChild(generateThumbnail(product));
+    productElement.appendChild(generateProductInfo(productName));
+    productElement.appendChild(generatePrice(product));
+
+    productWrapperElement.appendChild(productElement);
+    searchResultElement.appendChild(productWrapperElement);
+}
+
+window.addEventListener("load", function () {
+    let searchResultElement = document.getElementById("search-result");
+    let search = decodeURIComponent(window.location.href.split("?")[1]);
+    for (let [name, product] of Object.entries(products)) {
+        if (name.toLowerCase().includes(search)) {
+            addSearchResult(name, product, searchResultElement);
+        }
+    }
+});
