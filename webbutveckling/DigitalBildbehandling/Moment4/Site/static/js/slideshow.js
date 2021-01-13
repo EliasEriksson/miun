029f0aee798a32bfaf4e9646077c3d99965a4f56
EliasEriksson <EliasEriksson95@gmail.com>
2021-01-13 20:13:26 +0100

    changes

--- webbutveckling/DigitalBildbehandling/Moment4/Site/static/js/slideshow.js
+++ webbutveckling/DigitalBildbehandling/Moment4/Site/static/js/slideshow.js
@@ -1,15 +1,15 @@
 
-// $("#popular-glasses-slideshow > a:gt(0)").hide();
-//
-// setInterval(function() {
-//     $(
-//         "#popular-glasses-slideshow > a:first"
-//     ).fadeOut(
-//         1500
-//     ).next(
-//     ).fadeIn(
-//         1500
-//     ).end(
-//
-//     ).appendTo("#popular-glasses-slideshow");
-// }, 4000);
+$("#popular-glasses-slideshow > a:gt(0)").hide();
+
+setInterval(function() {
+    $(
+        "#popular-glasses-slideshow > a:first"
+    ).fadeOut(
+        1500
+    ).next(
+    ).fadeIn(
+        1500
+    ).end(
+
+    ).appendTo("#popular-glasses-slideshow");
+}, 4000);
