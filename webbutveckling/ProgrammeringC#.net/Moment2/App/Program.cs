1894e50720b50018387b70ce70c011a4d8774fce
EliasEriksson <EliasEriksson95@gmail.com>
2021-11-02 15:56:40 +0100

    removed files that are supposedto be ignored

--- webbutveckling/ProgrammeringC#.net/Moment2/App/Program.cs
+++ webbutveckling/ProgrammeringC#.net/Moment2/App/Program.cs
@@ -32,12 +32,17 @@ namespace App
          */
         private static (int, int, int) ReadInput(string[] args)
         {
+            if (args.Length != 3) throw new ParseException();
             
-            if (args.Length == 3)
-            {
-                return (Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), Convert.ToInt32(args[2]));
-            }
-            throw new ParseException();
+            var year = Convert.ToInt32(args[0]);
+            var month = Convert.ToInt32(args[1]);
+            var day = Convert.ToInt32(args[2]);
+
+            if (month is <= 0 or > 12) throw new ParseException();
+            if (day is <= 0 or > 31) throw new ParseException();
+            
+            return (year, month, day);
+
         }
 
         /**
@@ -48,7 +53,6 @@ namespace App
             var century = year / 100;
             year %= 100;
             var dayOfWeek = (day + ((13 * (month + 1)) / 5) + year + (year / 4) + (century / 4) + 5 * century) % 7;
-            Console.WriteLine(dayOfWeek);
             Console.WriteLine($"You were born on a {DayMap[dayOfWeek]}.");
         }
 
@@ -64,18 +68,20 @@ namespace App
             }
             catch (OverflowException)
             {
+                // number given from command line cant fit into a 32 bit int.
                 Console.WriteLine("You are either VERY VERY old or VERY VERY young.");
             }
             catch (FormatException)
             {
+                // there was not only numbers. maybe symbols or text was accidentally typed.
                 Console.WriteLine("You didnt only provide numbers.");
             }
             catch (ParseException)
             {
+                // generally end up here if there is too few or too many arguments.
                 Console.WriteLine("Please provide year month and day as sys arguments on format: yyyy mm dd");
                 return 1;
             }
-
             return 0;
         }
     }
