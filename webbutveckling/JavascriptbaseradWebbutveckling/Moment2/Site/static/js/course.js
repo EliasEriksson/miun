7a5b9358c803fe72386f4f44838730fa6ad42465
EliasEriksson <EliasEriksson95@gmail.com>
2021-11-17 11:52:58 +0100

    added comments and changed redirect to rootURL instead of ../../

--- webbutveckling/JavascriptbaseradWebbutveckling/Moment2/Site/static/js/course.js
+++ webbutveckling/JavascriptbaseradWebbutveckling/Moment2/Site/static/js/course.js
@@ -1,5 +1,6 @@
 import {requestEndpoint} from "./modules/requests.js";
 import {redirect} from "./modules/url.js";
+import {getCookies} from "./modules/cookie.js";
 
 const form = document.getElementById("course-form");
 const courseIdElement = form.querySelector("[name=courseId]");
@@ -9,6 +10,11 @@ const putElement = form.querySelector("[name=update-button]");
 const delElement = form.querySelector("[name=delete-button]");
 
 
+/**
+ * searches the URL for an id.
+ *
+ * @returns {number}
+ */
 const getIdFromURL = () => {
     let parts = location.href.split("/");
     if (parts[parts.length - 1]) {
@@ -17,6 +23,12 @@ const getIdFromURL = () => {
     return parseInt(parts[parts.length - 2]);
 }
 
+
+/**
+ * Reads data from DOM elements to an object.
+ *
+ * @returns {{courseName, coursePeriod, courseId}}
+ */
 const getCurrentFormCourse = () => {
     return {
         courseId: courseIdElement.value,
@@ -25,38 +37,56 @@ const getCurrentFormCourse = () => {
     }
 }
 
+
+/**
+ * Sends a PUT request to the course endpoint.
+ *
+ * @returns {number}
+ */
 const put = async () => {
     let id = getIdFromURL();
-    let [data, status] = await requestEndpoint(`/course/${id}/`, null, "put", {
+    let [_, status] = await requestEndpoint(`/course/${id}/`, null, "put", {
         ...getCurrentFormCourse()
     });
-    console.log("updated to:", data);
+    return status;
 }
 
+
+/**
+ * Sends a DELETE request to the course endpoint.
+ *
+ * @returns {number}
+ */
 const del = async () => {
     let id = getIdFromURL();
-    let [data, status] = await requestEndpoint(`/course/${id}/`, null, "delete");
-    console.log("deleted:", data);
+    let [_, status] = await requestEndpoint(`/course/${id}/`, null, "delete");
+    return status;
 }
 
 window.addEventListener("load", async () => {
+    let cookies = getCookies();
+    // prevent the form from being submitted
     form.addEventListener("submit", async e => {
         e.preventDefault();
     });
 
+    // sends the PUT request and if it is successful
+    // redirect back to root
     putElement.addEventListener("click", async e => {
         e.preventDefault();
-        await put();
-        redirect("../../");
+        let status = await put();
+        if (200 <= status && status < 300) {
+            redirect(cookies["rootURL"]);
+        }
     });
 
+    // sends the DELETE request and if it is successful
+    // redirect back to root
     delElement.addEventListener("click", async e => {
         e.preventDefault();
-        await del();
-        redirect("../../");
+        let status = await del();
+        if (200 <= status && status < 300) {
+            redirect(cookies["rootURL"]);
+        }
     });
-
-    fetch("asdasd").then((response) => {
-
-    })
 });
