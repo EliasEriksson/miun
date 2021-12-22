c17fc733febdb6ca3aaf4c41008907ac686ddad9
EliasEriksson <EliasEriksson95@gmail.com>
2021-12-22 22:43:33 +0100

    implemented basic CRUD functionality. initiated react project with proxy.

---
+++ webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/index.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const fs = require("fs").promises;
+const mongoose = require("mongoose");
+const models = require("./modules/models");
+
+const rootURL = "/jsweb/moment5";
+const apiRoot = `${rootURL}/api`;
+
+const app = express();
+app.use(express.json());
+app.use(rootURL, express.static(`${__dirname}/static`));
+
+app.get(`${apiRoot}/ingredients/`, async (request, response, next) => {
+    try {
+        await response.status(200).json(await models.ingredient.find());
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.get(`${apiRoot}/ingredients/:id/`, async (request, response, next) => {
+    try {
+        const ingredient = await models.ingredient.findById(request.params.id);
+        await response.status(200).json(ingredient);
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.post(`${apiRoot}/ingredients/`, async (request, response, next) => {
+    try {
+        const ingredient = new models.ingredient(request.body);
+        await ingredient.save();
+        response.status(201).json(ingredient);
+        next();
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.put(`${apiRoot}/ingredients/:id`, async (request, response, next) => {
+    try {
+        const ingredient = await models.ingredient.findByIdAndUpdate(request.params.id, request.body);
+        response.status(200).json(ingredient);
+        next();
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.delete(`${apiRoot}/ingredients/:id`, async (request, response, next) => {
+    try {
+        console.log(request.params.id)
+        const ingredient = await models.ingredient.findByIdAndDelete(request.params.id);
+        response.status(200).json(ingredient);
+        next();
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.get(`${apiRoot}/recipes/`, async (request, response, next) => {
+    try {
+        response.status(200).json(await models.recipe.find().populate("ingredients.ingredient"));
+        next();
+    } catch (e) {
+        next(e);
+    }
+
+});
+
+app.get(`${apiRoot}/recipes/:id`, async (request, response, next) => {
+    try {
+        const recipe = await models.recipe.findById(request.params.id).populate("ingredients.ingredient");
+        await response.status(200).json(recipe);
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.post(`${apiRoot}/recipes/`, async (request, response, next) => {
+    try {
+        console.log(request.body)
+        const recipe = new models.recipe(request.body);
+        console.log(recipe)
+        await recipe.save();
+        response.status(201).json(recipe);
+        next();
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.put(`${apiRoot}/recipes/:id`, async (request, response, next) => {
+    try {
+        const recipe = await models.recipe.findByIdAndUpdate(request.params.id, request.body);
+        response.status(200).json(recipe);
+        next()
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.delete(`${apiRoot}/recipes/:id`, async (request, response, next) => {
+    console.log("was here")
+    try {
+        const recipe = await models.recipe.findByIdAndDelete(request.params.id)
+        response.status(200).json(recipe);
+        next();
+    } catch (e) {
+        next(e);
+    }
+});
+
+const readCredentials = async () => {
+    return await JSON.parse(await fs.readFile(".credentials.json", "utf-8"));
+}
+
+app.get(`${apiRoot}`, async (request, response, next) => {
+    try {
+        await response.json({"message": "hello world"});
+    } catch (e) {
+        next(e);
+    }
+});
+
+const PORT = 8083;
+readCredentials().then(async cred => {
+    const mongoConnect = `mongodb://${cred.user}:${cred.pwd}@${cred.host}:${cred.port}/${cred.db}`;
+    console.log(`Connecting to mongo db...`)
+    await mongoose.connect(mongoConnect);
+    console.log("Connected to mongodb!")
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}${rootURL}`);
+    });
+});
+
+
---
+++ webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/modules/models.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+
+// const Ingredient = mongoose.model("Ingredient", new mongoose.Schema({
+//     ingredient: {
+//         type: [String],
+//         required: true,
+//         unique: true
+//     }
+// }, {strict: "throw"}));
+//
+// const Recipe = mongoose.model("Recipe", new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     ingredients: {
+//         type: [
+//             {
+//                 type: {
+//                     "ingredient": {
+//                         type: mongoose.Schema.Types.ObjectId,
+//                         ref: "Ingredient"
+//                     },
+//                     "amount": Number
+//                 },
+//                 required: true
+//             }
+//         ],
+//         required: true
+//     },
+//     instructions: {
+//         type: [String],
+//         required: true
+//     }
+// }, {strict: "throw"}));
+
+class Ingredient extends mongoose.model("Ingredient", new mongoose.Schema({
+    ingredient: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {strict: "throw"})) {
+    save = async (callback) => {
+        super.save();
+        if (callback) {
+            await callback();
+        }
+    }
+}
+
+
+class Recipe extends mongoose.model("Recipe", new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: [
+            {
+                type: {
+                    "ingredient": {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Ingredient"
+                    },
+                    "amount": Number
+                },
+                required: true
+            }
+        ],
+        required: true
+    },
+    instructions: {
+        type: [String],
+        required: true
+    }
+}, {strict: "throw"})) {
+    save = async (callback) => {
+        await super.save();
+
+        if (callback) {
+            await callback();
+        }
+    }
+}
+
+module.exports = {
+    "ingredient": Ingredient,
+    "recipe": Recipe
+};
