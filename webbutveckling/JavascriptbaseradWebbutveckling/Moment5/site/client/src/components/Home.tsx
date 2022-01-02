aa693ee2e9c05c40f6549449f337ab4af3a0516f
eliaseriksson <eliaseriksson95@gmail.com>
2022-01-02 01:06:38 +0100

    rewrote class components to function components. added delay to express start command to try preventing nodemon from starting server too early

--- webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/client/src/components/Home.tsx
+++ webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/client/src/components/Home.tsx
@@ -1,68 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import {ApiResponse, requestEndpoint} from "../modules/requests";
 import {Loader} from "./Loader";
 import {RecipeData} from "../types";
 import {RecipeSummary} from "./RecipeSummary";
 
-export class Home extends React.Component<{}, {
-    recipes: JSX.Element[], fetchedAll: boolean
-}> {
-    private fetching: boolean;
-    private fetchMore: boolean;
-    private nextApiPage: number | null;
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            recipes: [],
-            fetchedAll: false
-        }
-        this.fetching = false;
-        this.fetchMore = true;
-        this.nextApiPage = 1;
-    }
+let fetching = false;
+let fetchMore = true;
+let nextApiPage: number | null = 1;
 
-    fetchContent = async () => {
-        this.fetching = true;
-        if (this.nextApiPage) {
-            const data = await requestEndpoint<ApiResponse<RecipeData>>(`/recipes/?page=${this.nextApiPage}`);
 
-            await this.setState(({
-                recipes: [...this.state.recipes, ...data.docs.map((recipeData) => {
-                    return (<RecipeSummary
-                            key={recipeData._id}
-                            data={recipeData}/>
-                    );
-                })]
-            }));
-            this.nextApiPage = data.nextPage;
-            if (!this.nextApiPage) await this.setState(({fetchedAll: true}));
-            this.fetching = false;
-        }
+export const Home = () => {
+    const [state, setState] = useState<{ recipes: JSX.Element[], fetchedAll: boolean }>({
+        recipes: [],
+        fetchedAll: false
+    });
 
-    }
-
-    handleViewportEnter = async () => {
-        this.fetchMore = true;
-        while (this.fetchMore && this.nextApiPage) {
-            await this.fetchContent();
-        }
-    }
-
-    handleViewportLeave = async () => {
-        this.fetchMore = false;
-    }
-
-    render = () => {
-        return (
-            <main>
-                <div>
-                    {this.state.recipes}
-                </div>
-                {!this.state.fetchedAll ? <Loader
-                    onEnterViewport={this.handleViewportEnter}
-                    onLeaveViewport={this.handleViewportLeave}/> : null}
-            </main>
-        );
-    }
+    return (
+        <main>
+            <div>{state.recipes}</div>
+            {!state.fetchedAll ? <Loader
+                onEnterViewport={async () => {
+                    fetchMore = true;
+                    while (fetchMore && nextApiPage) {
+                        fetching = true;
+                        if (nextApiPage) {
+                            const data = await requestEndpoint<ApiResponse<RecipeData>>(
+                                `/recipes/?page=${nextApiPage}`
+                            );
+                            data.docs.forEach(recipeData => {
+                                state.recipes.push((
+                                    <RecipeSummary key={recipeData._id} data={recipeData} />
+                                ))
+                            })
+                            await setState({
+                                ...state,
+                            })
+                            nextApiPage = data.nextPage;
+                            if (!nextApiPage) await setState({...state, fetchedAll: true});
+                            fetching = false;
+                        }
+                    }
+                }}
+                onLeaveViewport={async () => {
+                        fetchMore = false;
+                }}/> : null}
+        </main>
+    )
 }
--- webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/client/src/components/Loader.tsx
+++ webbutveckling/JavascriptbaseradWebbutveckling/Moment5/site/client/src/components/Loader.tsx
@@ -3,10 +3,8 @@ import loading from "../loading.svg";
 import handleViewport from "react-in-viewport";
 
 
-class InnerLoader extends React.Component<{forwardedRef: React.Ref<any>}> {
-    render = () => {
-        return (<img ref={this.props.forwardedRef} src={loading} alt={"loading more content"}/>);
-    }
-}
-
-export const Loader = handleViewport(InnerLoader);
+export const Loader = handleViewport((props: { inViewport: boolean, forwardedRef: React.Ref<any> }) => {
+    return (
+        <img ref={props.forwardedRef} src={loading} alt={"loading more content"}/>
+    );
+});
